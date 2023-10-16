899393d4e07f4e004cd25afa7d92235439f18682
kotathefriends <5070kouta@gmail.com>
2023-10-16 22:46:49 +0900

    Initial Commit

---
+++ Dialog, Alert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dialog, Alert
+//
+//  Created by KOTA TAKAHASHI on 2023/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dialog, Alert/Dialog__AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dialog__AlertApp.swift
+//  Dialog, Alert
+//
+//  Created by KOTA TAKAHASHI on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct Dialog__AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
